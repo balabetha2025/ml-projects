43d27d63178439eb986472dd9fa3558a6cc752e9
Bala Betha <sairathan201820@gmail.com>
2025-09-17 08:20:21 +0530

    hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -27,19 +27,30 @@ def evaluate_model(x_train, y_train, x_test, y_test, models,param):
     try:
         report = {}
 
-        for name, model in models.items():
-            model.fit(x_train, y_train)
+        for i in range(len(list(models))):
+            model= list(models.values())[i] 
             para=param[list(models.keys())[i]]
 
-            gs = GridsearchCV(model,para,cv=3,n_jobs=n_jobs,verbose=verbose,refit=refit)
+            gs = GridSearchCV(model,para,cv=3,n_jobs=-1,verbose=1,refit=True)
             gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
 
             y_test_pred = model.predict(x_test)
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[name] = test_model_score
+            report[list(models.keys())[i]] = (test_model_score,gs.best_params_)
 
         return report
     
     except Exception as e:
         raise CustomException(f"Error while evaluating models: {e}", sys) from e
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(f"Failed to load object from {file_path}: {e}", sys) from e
+        
